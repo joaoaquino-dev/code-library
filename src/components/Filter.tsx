2a40318c107b55f06e666793186d7cde7e544c50
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-02-18 19:34:07 -0300

    feat(filter): add props typing with title and placeholder

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,21 +1,24 @@
 import { Search } from "lucide-react";
 
-function Filter() {
+type FilterProps = {
+  title?: string;
+  placeholder?: string;
+};
+
+function Filter({ title, placeholder }: FilterProps) {
   return (
     <div className="flex flex-col items-center mt-10">
-      <p className="font-bold text-[#00A2F7] text-xl">
-        Busque o conteúdo que deseja assistir hoje
-      </p>
+      <p className="font-bold text-[#00A2F7] text-xl">{title}</p>
       <p className="text-xs text-[#00A2F7]">Conteúdo exclusivo todos os dias</p>
 
       <div className="flex gap-2 items-center mt-2">
         <input
           type="text"
-          placeholder="Curso de React"
-          className="bg-white border-[#F2F2F2] rounded-md w-153.75 h-8.75 text-md text-center px-2"
+          placeholder={placeholder}
+          className="bg-white border-[#f2f2f2] rounded-md w-153.75 h-8.75 text-md text-center px-2"
         />
 
-        <div className="h-8.75 w-8.75 flex justify-center items-center rounded-md bg-[#E1F4FF]">
+        <div className="h-8.75 w-8.75 flex justify-center items-center rounded-md bg-[#E1F4FF]  cursor-pointer">
           <Search size={22} className="text-[#00A2F7]" />
         </div>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,10 @@ createRoot(document.getElementById("root")!).render(
     <div className="flex flex-col h-screen bg-[#F4FBFF}">
       <Header />
       <div className="flex-1 max-w-300 w-full m-auto">
-        <Filter />
+        <Filter
+          title="Busque o conteúdo que deseja assistir hoje"
+          placeholder="Buscar curso"
+        />
       </div>
       <Footer />
     </div>
