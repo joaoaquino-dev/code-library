5f032a3d27420dd6e32e4b3c89f0856a85751981
joaoquino-dev <joaoaquino.dev@outlook.com>
2026-02-18 12:28:32 -0300

    finish Filter.tsx and stylization

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,15 +1,24 @@
+import { Search } from "lucide-react";
+
 function Filter() {
   return (
-    <div className="flex flex-col items-center">
-      <p className="font-bold text-[#00A2F7]">
+    <div className="flex flex-col items-center mt-10">
+      <p className="font-bold text-[#00A2F7] text-xl">
         Busque o conteúdo que deseja assistir hoje
       </p>
       <p className="text-xs text-[#00A2F7]">Conteúdo exclusivo todos os dias</p>
 
-      <input
-        type="text"
-        className="bg-white border-[#F2F2F2] rounded-md w-153.75 h-8.75"
-      />
+      <div className="flex gap-2 items-center mt-2">
+        <input
+          type="text"
+          placeholder="Curso de React"
+          className="bg-white border-[#F2F2F2] rounded-md w-153.75 h-8.75 text-md text-center px-2"
+        />
+
+        <div className="h-8.75 w-8.75 flex justify-center items-center rounded-md bg-[#E1F4FF]">
+          <Search size={22} className="text-[#00A2F7]" />
+        </div>
+      </div>
     </div>
   );
 }
