67d13c46439368954143c902702c6c3720490e22
Joao-Aquino25 <joaoovcarvalhoo@gmail.com>
2026-02-24 20:31:33 -0300

    feat(Card): add dynamic props and fix layout alignment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,17 @@
-const Card = () => {
+type CardProps = {
+  title: string;
+  img: string;
+};
+
+const Card = ({ title, img }: CardProps) => {
   return (
-    <div className="border w-full p-2">
-      <img
-        src="https://logospng.org/wp-content/uploads/react.png"
-        className="w-32 mx-auto"
-        alt=""
-      />
-      <p className="text-[#00A2F7] font-bold text-md mt-2">
-        Fundamentos do React.Js
-      </p>
-      <p className="text-[#00A2F7] text-md mt-1">9H</p>
-      <button className="bg-[#00A2F7] text-white w-full py-1 rounded-md cursor-pointer mt-1">
+    <div className="border w-full p-4 rounded-md border-[#00A2F7] flex flex-col">
+      <div className="flex flex-1 justify-center items-center">
+        <img src={img} className="w-32 mx-auto" alt="" />
+      </div>
+      <p className="text-[#00A2F7] font-bold text-md mt-2 mx-auto">{title}</p>
+      <p className="text-[#00A2F7] text-md mt-1 mb-2">9H</p>
+      <button className="bg-[#00A2F7] text-white w-full py-1 rounded-md cursor-pointer mt-auto">
         Inscreva-se
       </button>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -8,23 +8,29 @@ import Footer from "./components/Footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="flex flex-col h-screen bg-[#F4FBFF}">
+    <div className="flex flex-col h-screen bg-[#F4FBFF]">
       <Header />
       <main className="flex-1 max-w-300 w-full m-auto">
         <Filter
           title="Busque o conteúdo que deseja assistir hoje"
           placeholder="Buscar curso"
         />
-        <div className="bg-white rounded-md py-2 mt-4">
+        <div className="bg-white rounded-md py-4 mt-4">
           <p className="font-bold text-[#00A2F7] text-center">
             Nossos cursos mais buscados
           </p>
           <div className="grid grid-cols-5 gap-4 mt-3">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card
+              title="React.js"
+              img="https://toppng.com/uploads/preview/react-logo-icon-11609374122d9vkbptqap.png"
+            />
+            <Card title="Node.js" img="./node.png" />
+            <Card title="Next.js" img="./Next.png" />
+            <Card title="Tailwind CSS" img="./TailwindCSS.png" />
+            <Card
+              title="Vue.js"
+              img="https://toppng.com/uploads/preview/vuejs-logo-11609381682s9e74tuv0d.png"
+            />
           </div>
         </div>
       </main>
